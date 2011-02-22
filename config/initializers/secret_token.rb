9c51ce64c54b8b62bf089de38978c993abf3395e
Aaron Suggs <aaron@ktheory.com>
2011-02-21 21:35:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EulerStats::Application.config.secret_token = 'b12ac0022489f727e593768d770dfa0f56f19393acf8628bf2efb0afe75aa051843d0553646405303e4121753880c6294dd56228d8c242b796f1d9de0719a80f'
